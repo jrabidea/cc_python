cf64e25abc62d932a15be18e4543ef229945e4dc
Josh Rabideaux <jrabidea@dhcp-11-19.bravo>
2015-07-07 13:58:56 -0500

    Added logic for saving and loading game

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -11,31 +11,33 @@ from middle_panel_menu import *
 class CookieClicker(unittest.TestCase):
 
     minutes = 0
+    start_time = time.time()
 
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.base_url = "http://orteil.dashnet.org/cookieclicker/"
 
     def timer(self):
-        start_time = time.time()
-        elapsed_time = time.time() - start_time
-        if elapsed_time > 59:
-            self.minutes += self.minutes
-            start_time = time.time()
-
+        elapsed_time = time.time() - self.start_time
+        if elapsed_time > 60:
+            self.minutes += 1
+            self.start_time = time.time()
+        print elapsed_time
+        print self.minutes
 
     def test_play_cookie_clicker(self):
-        last_save = 0
         driver = self.driver
         driver.get(self.base_url)
         cookie_pane = CookiePanel(driver)
         mid_pane = Menu(driver)
         mid_pane.turn_off_graphics()
+        mid_pane.import_save()
         while True:
             self.timer()
             cookie_pane.click_cookie()
-
-
+            if self.minutes == 5:
+                mid_pane.save_game()
+                self.minutes = 0
 
     def tearDown(self):
         self.driver.quit()
--- middle_panel_menu.py
+++ middle_panel_menu.py
@@ -8,20 +8,19 @@ class Menu(BasePageObject):
     menu_modal = By.ID, "menu"
     menu = By.ID, "prefsButton"
     menu_close = By.XPATH, "//div[@onclick = 'Game.ShowMenu();']"
-    load_save_modal = "prompt"
-    text_area = "textareaPrompt"
+    load_save_modal = By.ID, "prompt"
+    text_area = By.ID, "textareaPrompt"
 
     def __init__(self, driver):
         super(Menu, self).__init__(driver)
 
     def save_game(self):
-        export_text_area = By.ID, "textareaPrompt"
         export_save = By.XPATH,"//a[@onclick = 'Game.ExportSave();']"
         all_done = By.ID, "promptOption0"
         self.find_element(self.menu).click()
         self.wait(self.menu_modal, 20)
         self.find_element(export_save).click()
-        self.wait(self.text_area)
+        self.wait(self.text_area, 20)
         with open("Cookie_Clicker.txt", "w+") as save:
             save.write(self.find_element(self.text_area).text)
         self.find_element(all_done).click()
@@ -34,15 +33,25 @@ class Menu(BasePageObject):
         self.wait(self.menu_close, 20)
         self.find_element(import_save).click()
         self.wait(self.text_area, 20)
-        with open("Cookie_Clicker.txt", "+r") as load:
-            code = load.read(self.text_area)
+        with open("Cookie_Clicker.txt", "r") as load:
+            code = load.read()
         self.find_element(self.text_area).send_keys(code)
+        self.find_element(load_button).click()
+        self.find_element(self.menu_close).click()
 
     def turn_off_graphics(self):
-        settings =  By.XPATH, "//*[@id = 'menu']//div[@class = 'listing'][5]"
-        settings_list = self.find_elements(settings)
-        self.find_element(self.menu).click();
-        self.wait(self.menu_modal, 20)
-        for d in settings_list:
-            d.click()
+        fancy_graphics = By.ID, "fancyButton"
+        particles_button = By.ID, "particlesButton"
+        milk_button = By.ID, "milkButton"
+        cursors_button = By.ID, "cursorsButton"
+        settings = [fancy_graphics, particles_button, milk_button, cursors_button]
+        self.find_element(self.menu).click()
+        self.wait(cursors_button, 20)
+        for d in settings:
+            self.find_element(d).click()
+        self.find_element(self.menu_close).click()
+
+
+
+
 
