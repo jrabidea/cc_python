8f206c1d72fd00e09a163e81929585358453f77d
Josh Rabideaux <jrabidea@dhcp-11-19.bravo>
2015-07-08 12:28:27 -0500

    Added limits to the amount of products bought

--- store_panel.py
+++ store_panel.py
@@ -8,17 +8,32 @@ class StoreUpgrades(BasePageObject):
 
     enabled = "product unlocked enabled"
     crate_enabled = "crate upgrade enabled"
+    limit = 10
+    limit_two = 10
+    total_products = 0
 
     def buy_products(self):
 
-        if self.get_class(0) == self.enabled and self.get_prod_owned(0) < 21:
+        if self.get_class(0) == self.enabled and self.get_prod_owned(0) < self.limit:
             self.click_upgrade(0)
-        elif self.get_class(1) == self.enabled and self.get_prod_owned(1) < 21:
+        elif self.get_class(1) == self.enabled and self.get_prod_owned(1) < self.limit:
             self.click_upgrade(1)
-        elif self.get_class(2) == self.enabled and self.get_prod_owned(2) < 21:
+        elif self.get_class(2) == self.enabled and self.get_prod_owned(2) < self.limit:
             self.click_upgrade(2)
-        elif self.get_class(3) == self.enabled and self.get_prod_owned(3) < 21:
+        elif self.get_class(3) == self.enabled and self.get_prod_owned(3) < self.limit:
             self.click_upgrade(3)
+        elif self.get_class(4) == self.enabled and self.get_prod_owned(4) < self.limit:
+            self.click_upgrade(4)
+        elif self.get_class(5) == self.enabled and self.get_prod_owned(5) < self.limit:
+            self.click_upgrade(5)
+        elif self.get_class(6) == self.enabled and self.get_prod_owned(6) < self.limit:
+            self.click_upgrade(6)
+        elif self.get_class(7) == self.enabled and self.get_prod_owned(7) < self.limit:
+            self.click_upgrade(7)
+        elif self.get_class(8) == self.enabled and self.get_prod_owned(8) < self.limit:
+            self.click_upgrade(8)
+        elif self.get_class(9) == self.enabled and self.get_prod_owned(9) < self.limit:
+            self.click_upgrade(9)
 
     def get_class(self, index):
 
@@ -30,7 +45,17 @@ class StoreUpgrades(BasePageObject):
 
         self.index = index
 
-        prod_owned = ["productOwned0", "productOwned1", "productOwned2", "productOwned3"]
+        prod_owned = ["productOwned0", "productOwned1", "productOwned2",
+                      "productOwned3", "productOwned4", "productOwned5",
+                      "productOwned6", "productOwned5", "productOwned6",
+                      "productOwned7", "productOwned8", "productOwned9"]
+
+        for a in prod_owned:
+            if self.driver.find_element_by_id(a).text == "":
+                self.total_products += 0
+            else:
+                bought_num = int(self.driver.find_element_by_id(a).text)
+                self.total_products += bought_num
 
         if self.driver.find_element_by_id(prod_owned[index]).text == "":
             return 0
@@ -45,7 +70,9 @@ class StoreUpgrades(BasePageObject):
 
         self.index = index
 
-        products = ["product0","product1","product2","product3"]
+        products = ["product0","product1","product2","product3", "product4",
+                    "product5", "product6", "product7", "product8",
+                    "product9"]
 
         return self.driver.find_element_by_id(products[index])
 
@@ -55,3 +82,15 @@ class StoreUpgrades(BasePageObject):
 
         if self.find_element(store_upgrade).get_attribute("className") == self.crate_enabled:
             self.find_element(store_upgrade).click()
+
+
+    def set_limit(self, total_products):
+
+        self.total_products = total_products
+
+        if total_products == 80:
+            limit = 20
+        elif total_products == 210:
+            limit = 50
+        elif total_products == 490:
+            limit = 100
